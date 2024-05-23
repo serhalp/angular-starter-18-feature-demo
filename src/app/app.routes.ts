7975a56c3971803a09796a46950eb7690e2fbadf
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-23 10:37:01 -0400

    add Netlify Forms demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MyFormComponent } from './my-form/my-form.component';
+
+export const routes: Routes = [{ path: 'my-form', component: MyFormComponent }];
